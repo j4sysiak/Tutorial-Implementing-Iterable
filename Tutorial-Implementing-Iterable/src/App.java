90eebcc27159730bd2c6fb521f8d4f4d163852e0
j4sysiak <j4sysiak@gmail.com>
2019-03-14 06:48:48 +0100

    kk

--- Tutorial-Implementing-Iterable/src/App.java
+++ Tutorial-Implementing-Iterable/src/App.java
@@ -6,16 +6,24 @@ public class App {
 	public static void main(String[] args) {
 	 
 		UrlLibrary urlLibrary = new UrlLibrary();
+		   
+		/*Custom zaimplementowany w klasie UrlLibrary*/
+		for(String x : urlLibrary){
+			System.out.println(x);
+		}
 		
+		//--lub
+		
+		
+		/*standardowy zaimplementowany w Collections*/
 		Iterator<String> it = urlLibrary.iterator();
 		
 		while(it.hasNext()){
 			String value = it.next();
 			System.out.println(value);
 		}
-
+ 
 	}
-
 }
 
 
